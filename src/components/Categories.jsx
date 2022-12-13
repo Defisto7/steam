79b5c801a06f5cd34e4c9b150e7974d8e231797a
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-12-13 11:30:55 +0200

    Master
    add categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 const Categories = () => {
   return (
-    <div>Categories</div>
-  )
+    <div className='flex justify-between gradient mx-[2.5rem] px-4 mt-8'>
+      <ul className="text-white flex items-center gap-8 py-1.5 text-[13px]">
+        <li>
+          <p>Your Store</p>
+        </li>
+        <li>
+          <p>New</p>
+        </li>
+        <li>
+          <p>Categories</p>
+        </li>
+        <li>
+          <p>Points Shop</p>
+        </li>
+        <li>
+          <p>News</p>
+        </li>
+        <li>
+          <p>Labs</p>
+        </li>
+      </ul>
+     <input type="search" placeholder="search" className="pl-4 pr-2 m-[1px] rounded-full placeholder:text-black bg-[#5a8cb6]"></input>
+    </div>
+  );
 }
 
 export default Categories
